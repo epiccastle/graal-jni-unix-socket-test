7da7c589fbb95e463aa8ff40d02ded3cf8afaf39
Crispin Wellington <retrogradeorbit@gmail.com>
2020-02-14 10:31:50 +0800

    revert order to broken order

--- src/SocketTest.c
+++ src/SocketTest.c
@@ -117,7 +117,7 @@ JNIEXPORT jint JNICALL Java_SocketTest_unix_1socket_1read
  * Signature: (I[BI)I
  */
 JNIEXPORT jint JNICALL Java_SocketTest_unix_1socket_1write
-  (JNIEnv *env, jclass this, jint fd, jint count, jbyteArray buf)
+  (JNIEnv *env, jclass this, jint fd, jbyteArray buf, jint count)
 {
   jboolean copy = 1;
   jbyte *buffer = (*env)->GetByteArrayElements(env, buf, &copy);
